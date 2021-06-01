a046b5bb4e003bbf739d269eabe41fda1da52e17
zouyuan <1946407125@qq.com>
2021-06-01 11:43:08 +0800

    普通用户--提交工单附带图片

--- src/views/userPage/AddOrder.vue
+++ src/views/userPage/AddOrder.vue
@@ -16,10 +16,22 @@
             <el-form-item label="维修地址" prop="address">
                 <el-input v-model="ruleForm.address" autocomplete="off" style="width: 800px"></el-input>
             </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="submitForm('ruleForm')">提交工单</el-button>
-            </el-form-item>
         </el-form>
+        <el-upload
+                action="aaaaaaa"
+                list-type="picture-card"
+                :on-preview="handlePictureCardPreview"
+                :on-remove="handleRemove"
+                :on-change="handleChange"
+                :auto-upload="false"
+                :multiple="multiple"
+                :file-list="fileList">
+            <i class="el-icon-plus"></i>
+        </el-upload>
+        <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt="">
+        </el-dialog>
+        <el-button type="primary" @click="submitForm('ruleForm')">提交工单</el-button>
     </div>
 </template>
 
@@ -28,8 +40,12 @@
         name: "AddOrder",
         data() {
             return {
-                loading:true,
+                fileList: [],
+                loading: true,
                 optionsType: [],
+                dialogImageUrl: '',
+                dialogVisible: false,
+                multiple: true,
                 ruleForm: {
                     type: null,
                     reason: null,
@@ -49,35 +65,68 @@
             }
         },
         methods: {
+            submitUpload() {
+                console.log(this.fileList)
+                let formData = new FormData();
+                this.fileList.forEach(item => {
+                    formData.append("files", item.raw);
+                });
+
+                axios.post('/user/uploadImg', formData).then(function (response) {
+                    console.log(response)
+                })
+            },
+            handleRemove(file, fileList) {
+                console.log(file, fileList);
+            },
+            handlePictureCardPreview(file) {
+                this.dialogImageUrl = file.url;
+                this.dialogVisible = true;
+            },
+            handleChange(file, fileList) {
+                this.fileList = fileList;
+            },
             submitForm(formName) {
                 this.$refs[formName].validate((valid) => {
                     if (valid) {
-                        let params = new URLSearchParams()
                         let that = this
                         that.loading = true
-                        let type = this.ruleForm.type
-                        let reason = this.ruleForm.reason
-                        let address = this.ruleForm.address
-                        const token = sessionStorage.getItem("token");
-                        params.append('token', token)
-                        params.append('type', type)
-                        params.append('reason', reason)
-                        params.append('address', address)
-                        axios.post('/user/addOrder', params).then(function (response) {
-                            that.loading = false
+                        //图片上传
+                        console.log(this.fileList)
+                        let formData = new FormData();
+                        this.fileList.forEach(item => {
+                            formData.append("files", item.raw);
+                        });
+                        axios.post('/user/uploadImg', formData).then(function (response) {
                             console.log(response)
-                            that.loading = false
-                            if (response.data.status === 444) {
-                                that.$message.error("您的登录信息已过期，请重新登录")
-                                that.$router.replace("/")
-                            } else if (response.data.status === 445) {
-                                that.$message.error("您没有此操作权限")
-                            } else {
-                                that.$message.success("工单提交成功")
-                                that.ruleForm.address = null
-                                that.ruleForm.reason = null
-                                that.ruleForm.type = null
-                            }
+                            let params = new URLSearchParams()
+                            let type = that.ruleForm.type
+                            let reason = that.ruleForm.reason
+                            let address = that.ruleForm.address
+                            // let imgsfileids1 = response.data
+                            let imgsfileids1 = JSON.stringify(response.data)
+                            console.log(imgsfileids1)
+                            const token = sessionStorage.getItem("token");
+                            params.append('token', token)
+                            params.append('type', type)
+                            params.append('reason', reason)
+                            params.append('address', address)
+                            params.append('imgsfileids1', imgsfileids1)
+                            axios.post('/user/addOrder', params).then(function (response) {
+                                that.loading = false
+                                console.log(response)
+                                that.loading = false
+                                if (response.data.status === 444) {
+                                    that.$message.error("您的登录信息已过期，请重新登录")
+                                    that.$router.replace("/")
+                                } else if (response.data.status === 445) {
+                                    that.$message.error("您没有此操作权限")
+                                } else {
+                                    that.$message.success("工单提交成功")
+                                    that.ruleForm.address = null
+                                    that.ruleForm.reason = null
+                                }
+                            })
                         })
                     } else {
                         console.log('error submit!!');
