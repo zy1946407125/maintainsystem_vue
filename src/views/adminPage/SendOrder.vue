1b44d55cc36b839dff51fca33f1d4f1c2e229e8b
zouyuan <1946407125@qq.com>
2021-06-01 20:02:25 +0800

    优化管理员派单选择工人页

--- src/views/adminPage/SendOrder.vue
+++ src/views/adminPage/SendOrder.vue
@@ -62,7 +62,9 @@
                                 title="确认取消工单吗？"
                                 @confirm="cancelOrder(scope.row)"
                         >
-                            <el-button :disabled="scope.row.status!=='未派单'" slot="reference" icon="el-icon-s-promotion">撤单</el-button>
+                            <el-button :disabled="scope.row.status!=='未派单'" slot="reference" icon="el-icon-s-promotion">
+                                撤单
+                            </el-button>
                         </el-popconfirm>
                     </template>
                 </el-table-column>
@@ -73,6 +75,48 @@
                         <el-image :src="item" fit="contain"></el-image>
                     </el-carousel-item>
                 </el-carousel>
+            </el-dialog>
+            <el-dialog title="选择维修人员" :visible.sync="dialogFormVisible2">
+                <div>
+                    <el-table
+                            :data="tableData2"
+                            border
+                            style="width: 100%">
+                        <el-table-column
+                                fixed="left"
+                                prop="id"
+                                label="编号"
+                                width="150">
+                        </el-table-column>
+                        <el-table-column
+                                prop="username"
+                                label="姓名"
+                                width="120">
+                        </el-table-column>
+                        <el-table-column
+                                prop="type"
+                                label="工作类型"
+                                width="120">
+                        </el-table-column>
+                        <el-table-column
+                                prop="phone"
+                                label="联系方式"
+                                width="120">
+                        </el-table-column>
+                        <el-table-column
+                                label="操作"
+                                width="200">
+                            <template slot-scope="scope">
+                                <el-popconfirm
+                                        title="确认向其派单吗？"
+                                        @confirm="send(scope.row)"
+                                >
+                                    <el-button slot="reference" icon="el-icon-s-promotion">派单</el-button>
+                                </el-popconfirm>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                </div>
             </el-dialog>
         </div>
     </div>
@@ -85,22 +129,24 @@
                 id: '',
                 size: 10,
                 tableData: null,
+                tableData2: null,
                 total: null,
                 loading: true,
                 dialogFormVisible: false,
+                dialogFormVisible2: false,
                 imgUrl: null,
             }
         },
         methods: {
-            cancelOrder(row){
+            cancelOrder(row) {
                 console.log(row)
                 const that = this
                 that.loading = true
                 var token = sessionStorage.getItem("token")
                 var params = new URLSearchParams()
                 params.append('token', token)
-                params.append('id',row.id)
-                axios.post('/admin/removeOrder',params).then(function (response) {
+                params.append('id', row.id)
+                axios.post('/admin/removeOrder', params).then(function (response) {
                     console.log(response)
                     that.loading = false
                     console.log(response)
@@ -109,22 +155,71 @@
                         that.$router.replace("/")
                     } else if (response.data.status === 445) {
                         that.$message.error("您没有此操作权限")
-                    } else if(response.data===1){
+                    } else if (response.data === 1) {
                         that.$message.success("撤单成功")
                         that.select()
-                    }else {
+                    } else {
                         that.$message.error("撤单失败")
                     }
                 })
             },
+            send(row) {
+                const that = this
+                that.loading = true
+                console.log(row)
+                const token = sessionStorage.getItem("token")
+                var params = new URLSearchParams()
+                params.append('token', token)
+                params.append('id', that.id)
+                params.append('wid', row.id)
+                console.log(that.id)
+                console.log(row.id)
+                axios.post('/admin/sendOrder', params)
+                    .then(function (response) {
+                        console.log(response)
+                        that.loading = false
+                        if (response.data.status === 444) {
+                            that.$message.error("您的登录信息已过期，请重新登录")
+                            that.$router.replace("/")
+                        } else if (response.data.status === 445) {
+                            that.$message.error("您没有此操作权限")
+                        } else if (response.data === 1) {
+                            that.$message.success("派单成功")
+                            that.dialogFormVisible2 = false
+                            that.select()
+                        } else {
+                            that.$message.error("未知错误")
+                        }
+                    })
+            },
             handleClick(row) {
                 console.log(row)
-                this.$router.push({
-                    path: '/admin/chooseWorker',
-                    query: {
-                        order: row
-                    }
-                })
+                // this.$router.push({
+                //     path: '/admin/chooseWorker',
+                //     query: {
+                //         order: row
+                //     }
+                // })
+                const that = this
+                that.loading = true
+                that.id = row.id
+                var token = sessionStorage.getItem("token")
+                var params = new URLSearchParams()
+                params.append('token', token)
+
+                axios.post('/admin/selectWorkers', params)
+                    .then(function (response) {
+                        that.loading = false
+                        if (response.data.status === 444) {
+                            that.$message.error("您的登录信息已过期，请重新登录")
+                            that.$router.replace("/")
+                        } else if (response.data.status === 445) {
+                            that.$message.error("您没有此操作权限")
+                        } else {
+                            that.tableData2 = response.data
+                            that.dialogFormVisible2 = true
+                        }
+                    })
             },
             select() {
                 const that = this
